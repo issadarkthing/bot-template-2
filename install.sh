158284d57b99550fd6cdd1dce97640de1a59db59
raziman <raziman@tutanota.com>
2023-03-13 00:29:36 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+sudo apt update
+sudo apt install git build-essential
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install 18
+nvm use 18
+
+
+npm install
+npm run build
+npm install -g pm2
+
+pm2 start dist/index.js
