43035b112bf67b80e61d8add1f3bed80bdde0455
raziman <raziman@tutanota.com>
2023-03-13 16:50:56 +0800

    fix install.sh does not cd into repo

--- install.sh
+++ install.sh
@@ -5,15 +5,18 @@ REPO=https://github.com/issadarkthing/bot-template-2.git
 sudo apt -y update
 sudo apt -y install git build-essential
 
-git clone $REPO
-
+# install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+# set node version
 nvm install 18
 nvm use 18
+
+git clone $REPO
+cd "$(basename "$REPO" .git)"
 
 npm install
 npm run build
